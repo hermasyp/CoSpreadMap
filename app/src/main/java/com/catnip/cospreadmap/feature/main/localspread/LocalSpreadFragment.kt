da1d9425daacf42be1a7f6a63eea671d80d3ef51
Hermas <hermas.yuda@gmail.com>
2020-05-28 23:37:43 +0700

    finishing simple list in local spread

--- app/src/main/java/com/catnip/cospreadmap/feature/main/localspread/LocalSpreadFragment.kt
+++ app/src/main/java/com/catnip/cospreadmap/feature/main/localspread/LocalSpreadFragment.kt
@@ -5,10 +5,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.catnip.cateringlist.utils.result.ResultState
 import com.catnip.cospreadmap.R
+import com.catnip.cospreadmap.ui.adapter.LocalSpreadListAdapter
+import kotlinx.android.synthetic.main.fragment_local.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -16,6 +20,7 @@ class LocalSpreadFragment : Fragment() {
 
     private val localSpreadViewModel: LocalSpreadViewModel by viewModel()
     private val TAG = LocalSpreadFragment::class.java.simpleName
+    private lateinit var listAdapter: LocalSpreadListAdapter
 
 
     override fun onCreateView(
@@ -28,13 +33,24 @@ class LocalSpreadFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        listAdapter = LocalSpreadListAdapter {
+            Toast.makeText(context, it.province.name, Toast.LENGTH_SHORT).show()
+        }
+        rv_local_spread?.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = listAdapter
+            setHasFixedSize(true)
+        }
         localSpreadViewModel.localData.observe(viewLifecycleOwner, Observer {
             when (it) {
                 is ResultState.Progress -> {
                     Log.d(TAG, "On progress")
                 }
                 is ResultState.Success -> {
-
+                    it.data.spreadData?.let { list ->
+                        listAdapter.items = list
+                    }
                 }
                 is ResultState.Error -> {
                     Log.d(TAG, "Error")
---
+++ app/src/main/java/com/catnip/cospreadmap/ui/adapter/LocalSpreadListAdapter.kt
@@ -0,0 +1,50 @@
+package com.catnip.cospreadmap.ui.adapter;
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.catnip.cospreadmap.R
+import com.catnip.cospreadmap.data.model.spread.local.LocalSpreadWrapper
+import kotlinx.android.synthetic.main.item_local_spread.view.*
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+class LocalSpreadListAdapter(private val itemClick: (LocalSpreadWrapper) -> Unit) :
+    RecyclerView.Adapter<LocalSpreadListAdapter.LocalSpreadViewHolder>() {
+    var items: List<LocalSpreadWrapper> = mutableListOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        LocalSpreadViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_local_spread, parent, false),
+            itemClick
+        )
+
+
+    override fun onBindViewHolder(holder: LocalSpreadViewHolder, position: Int) {
+        holder.bindView(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class LocalSpreadViewHolder(view: View, val itemClick: (LocalSpreadWrapper) -> Unit) :
+        RecyclerView.ViewHolder(view) {
+        fun bindView(item: LocalSpreadWrapper) {
+            with(item) {
+                itemView.setOnClickListener { itemClick(this) }
+                itemView.tv_province_name?.text = this.province.name
+                itemView.tv_positive_count?.text = this.localSpread?.positive
+                itemView.tv_cured_count?.text = this.localSpread?.cured
+                itemView.tv_death_count?.text = this.localSpread?.death
+            }
+
+        }
+    }
+
+}
